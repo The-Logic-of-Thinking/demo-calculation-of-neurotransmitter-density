f440a266bc594aff0b10b25d3f9af4668d887d05
Dmitriy Shabanov <shabanovd@gmail.com>
2014-07-22 14:25:22 +0400

    code

---
+++ calculation of neurotransmitter density/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 34)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Шаг"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(197, 36)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(42, 318)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(120, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "0"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(245, 36)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(42, 318)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(293, 36)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(42, 318)
+        Me.TextBox3.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(13, 73)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "100 шагов"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(341, 36)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(49, 318)
+        Me.TextBox4.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 152)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(102, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Новый паттерн"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(13, 112)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "4000"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(13, 209)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(140, 195)
+        Me.TextBox5.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(397, 458)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+
+End Class
---
+++ calculation of neurotransmitter density/Form1.vb
@@ -0,0 +1,191 @@
+﻿Public Class Form1
+
+    Dim NSt = 0
+
+    Dim Nsource As Integer = 25000
+
+    Dim Nneuron As Integer = 650
+
+    Dim Ncatch As Integer = 15
+    Dim RSC1 As Integer
+    Dim RSC2 As Integer
+
+
+    Dim NSignals As Integer = 10
+
+
+
+
+    Dim Dendrit(Nsource - 1) As Integer
+
+    Dim Pattern(Nneuron - 1) As Boolean
+
+    Dim AVG(Ncatch - 1) As Integer
+
+    Dim DistrP(Ncatch - 1) As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+        TextBox5.Text = "Nsource=" & Nsource.ToString & vbCrLf & "Nneuron=" & Nneuron.ToString & vbCrLf & "Ncatch =" & Ncatch.ToString & vbCrLf & "Nsig =" & NSignals.ToString
+
+
+        If Ncatch Mod 2 = 0 Then
+
+            RSC1 = Ncatch / 2
+            RSC2 = RSC1 - 1
+
+
+        Else
+
+            RSC1 = (Ncatch - 1) / 2
+            RSC2 = RSC1
+
+        End If
+
+
+
+        ConnectionCreate()
+
+    End Sub
+
+    Private Sub ConnectionCreate()
+
+        For i = 0 To Nsource - 1
+
+            Dendrit(i) = Rnd() * (Nneuron - 1)
+
+        Next
+
+    End Sub
+
+    Private Sub PatternCreate()
+
+        Dim N, k As Integer
+
+        For i = 0 To Nneuron - 1
+
+            Pattern(i) = False
+
+        Next
+
+        k = 0
+
+        Do While k < NSignals
+
+            N = Rnd() * (Nneuron - 1)
+
+            If Not Pattern(N) Then
+                Pattern(N) = True
+                k += 1
+            End If
+
+        Loop
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        St()
+    End Sub
+
+    Private Sub St()
+
+        PatternCreate()
+
+        Dim result(Nsource - 1) As Integer
+        Dim Distr(Ncatch - 1) As Integer
+
+        Dim C As Integer = 0
+
+        For i = 0 To Ncatch - 1
+            If Pattern(Dendrit(i)) Then C += 1
+        Next
+
+        For i = RSC1 To Nsource - RSC2 - 1
+
+            result(i) = C
+
+            Distr(C) += 1
+
+            If Pattern(Dendrit(i - RSC1)) Then C -= 1
+            If Pattern(Dendrit(i + RSC2)) Then C += 1
+
+        Next
+
+        For i = 0 To Ncatch - 1
+
+            If Distr(i) > 0 Then DistrP(i) += 1
+
+        Next
+
+        For i = 0 To Ncatch - 1
+
+            AVG(i) += Distr(i)
+
+        Next
+
+        NSt += 1
+        Label1.Text = NSt.ToString
+        Label1.Update()
+
+        Dim str1 As String = ""
+        Dim str2 As String = ""
+        Dim str3 As String = ""
+        Dim str4 As String = ""
+
+        For i = 0 To Ncatch - 1
+
+            str1 &= i.ToString & vbCrLf
+            str2 &= Distr(i).ToString & vbCrLf
+
+            If DistrP(i) <> 0 Then
+                str3 &= ((AVG(i) / DistrP(i)).ToString & "     ").Substring(0, 5) & vbCrLf
+            Else
+                str3 &= ("0" & vbCrLf)
+            End If
+
+
+            str4 &= ((DistrP(i) / NSt).ToString & "     ").Substring(0, 5) & vbCrLf
+
+        Next
+
+        TextBox1.Text = str1
+        TextBox1.Update()
+        TextBox2.Text = str2
+        TextBox2.Update()
+        TextBox3.Text = str3
+        TextBox3.Update()
+        TextBox4.Text = str4
+        TextBox4.Update()
+
+    End Sub
+
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        NSt = 0
+        For i = 0 To Ncatch - 1
+
+            DistrP(i) = 0
+
+        Next
+
+        PatternCreate()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For i = 0 To 99
+            St()
+        Next
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        For i = 0 To 3999
+            St()
+        Next
+    End Sub
+End Class
